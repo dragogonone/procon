d9573674f4bcfa528cde9f8110f05a9f676c0ebb
Ryuichi Yata <dragogonone@gmail.com>
2017-10-11 11:46:08 +0900

    class ni sita

--- union_find.py
+++ union_find.py
@@ -1,21 +1,31 @@
-# 素集合データ構造
-parent = [i for i in range(N)]
-rank = [0 for i in range(N)]
- 
-def find(v):
-    if parent[v] != v:
-        parent[v] = find(parent[v])
-    return parent[v]
- 
-def union(a, b):
-    rootA = find(a)
-    rootB = find(b)
- 
-    if rootA == rootB:
-        return
-    if rank[rootA] < rank[rootB]:
-        parent[rootA] = rootB
-    else:
-        parent[rootB] = rootA
-        if rank[rootA] == rank[rootB]:
-            rank[rootA] += 1
+class UnionFind:
+    def __init__(self, _n):
+        # 素集合データ構造
+        self.parent = [i for i in range(_n)]
+        self.rank = [0 for _ in range(_n)]
+
+    def find(self, v):
+        if self.parent[v] != v:
+            self.parent[v] = self.find(self.parent[v])
+        return self.parent[v]    
+
+    def union(self, a, b):
+        rootA = self.find(a)
+        rootB = self.find(b)
+        if rootA == rootB:
+            return
+
+        if self.rank[rootA] < self.rank[rootB]:
+            self.parent[rootA] = rootB
+        else:
+            self.parent[rootB] = rootA
+            if self.rank[rootA] == self.rank[rootB]:
+                self.rank[rootA] += 1
+
+if __name__ == '__main__':
+    u = UnionFind(5)
+    u.union(2, 4)
+    u.union(1, 3)
+    u.union(2, 3)
+    print(u.parent)
+    print(u.rank)
